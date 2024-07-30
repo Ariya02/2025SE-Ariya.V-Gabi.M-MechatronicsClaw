18d08bf4fa9397f9b0277d9dc422eb2f39789194
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-07-31 09:40:07 +1000

    LED Array work in progress

--- Mechatronics Sorting Claw/LED_Array/LED_Array.ino
+++ Mechatronics Sorting Claw/LED_Array/LED_Array.ino
@@ -1,16 +1,32 @@
 #define LED_PIN 13
 
-#include "LED_Array.h"
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
 
-Led led(LED_PIN);
+ArduinoLEDMatrix Led;
 
 void setup() {
-  led.init();
+  Serial.begin(9600);
+   Led.begin();
+   Led.textFont(Font_5x7);
+   Led.textScrollSpeed(100);
+   Led.stroke(0xFF, 0, 0);
+   Led.beginText(0, 1, 0xFF, 0, 0);
+   Led.print("V00.00.01");
+   Led.endText(SCROLL_LEFT);
 }
 
 void loop() {
-  led.on();
+  Led.loadFrame(arrow_left);
   delay(500);
-  led.off();
+
+  Led.loadFrame(arrow_right);
   delay(500);
+  
+  Led.loadFrame(arrow_up);
+  delay(500);
+
+  Led.loadFrame(arrow_down);
+  delay(500);
+
 }
