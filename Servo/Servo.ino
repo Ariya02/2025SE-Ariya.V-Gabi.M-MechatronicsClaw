affb1881d543f42fa4203f173576028ed27a36fe
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-07-31 09:31:59 +1000

    Servo Work in Progress
    Added methods, double check the correct methods are in before working next

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+class Servo
+{
+private: 
+  byte pin;
+public: 
+  Servo() {} // do not use
+
+  Servo(byte pin)
+  {
+    this->pin = pin;
+  }
+  void init()
+  {
+    pinMode(pin, OUTPUT);
+  }
+
+  void attach(pin)
+  {
+    pinMode(pin, OUTPUT);
+  }
+
+  void write(angle)
+  {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+
+
+void setup() {
+  myservo.attach(5, 700, 2300);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  myservo.write(0-180);
 }
