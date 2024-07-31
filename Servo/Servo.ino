9156c0ae2332fca8935e21d7d851a04f95e1a655
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-08-01 09:20:32 +1000

    Working on Servo

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#define SERVO_PIN 13 
 
 class Servo
 {
@@ -11,6 +12,7 @@ public:
   {
     this->pin = pin;
   }
+
   void init()
   {
     pinMode(pin, OUTPUT);
@@ -23,25 +25,28 @@ public:
 
   void write(angle)
   {
-    pinMode(pin, OUTPUT);
+    angle(int, OUTPUT);
   }
-}
+};
 
-
+Servo servo(SERVO_PIN);
+int pos = 0 ;
 
 void setup() {
-  myservo.attach(9);
+  servo.init();
+  servo.attach(9);
 }
 
-int pos = 0 
+
 void loop() {
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
+
