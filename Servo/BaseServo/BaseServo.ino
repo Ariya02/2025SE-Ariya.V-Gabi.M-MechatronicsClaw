0bec188ff9629fa6bd80a61bfdbbd7479ed69c9b
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-08-05 12:07:27 +1000

    Working Base Servo

--- Servo/BaseServo/BaseServo.ino
+++ Servo/BaseServo/BaseServo.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include "robotServo.h"
 
+myServo BaseServo(9);
+
+void setup() {
+  Serial.begin(9600);
+  BaseServo.init(90);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  int position = 0;
+for (position = 0; position <= 180; position += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    BaseServo.setPos(position);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (position = 180; position >= 0; position -= 1) { // goes from 180 degrees to 0 degrees
+    BaseServo.setPos(position);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
