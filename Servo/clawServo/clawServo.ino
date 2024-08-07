dc8d623e9bec97aa726cc73630b28a8f4b8a3a2f
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-08-07 13:32:30 +1000

    Working Claw

--- Servo/clawServo/clawServo.ino
+++ Servo/clawServo/clawServo.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include "robotServo.h"
 
+myServo ClawServo(9);
+
+void setup() {
+  Serial.begin(9600);
+  ClawServo.init(90);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  int position = 0;
+for (position = 30; position <= 100; position += 1) { // goes from 45 degrees to 90 degrees
+    // in steps of 1 degree
+    ClawServo.setPos(position);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (position = 100; position >= 30; position -= 1) { 
+    ClawServo.setPos(position);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
 }
