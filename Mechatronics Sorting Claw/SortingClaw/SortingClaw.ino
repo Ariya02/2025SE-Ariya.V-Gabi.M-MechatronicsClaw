cc00e4a91d2b130517562e6b442ddf519d90fe3f
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-08-08 13:20:28 +1000

    Files created for main robot

--- Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
+++ Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include "robotServo.h"
 
+myServo neckServo(9);
+
+void setup() {
+  Serial.begin(9600);
+  neckServo.init(90);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int pos = 0;
+  for (pos = 45; pos <= 180; pos += 1) { // goes from 45 degrees to 180 degrees
+      neckServo.setPos(pos);              // tell servo to go to position in variable 'pos'
+      delay(100);                       // waits 15ms for the servo to reach the position
+    }
+    for (pos = 180; pos >= 45; pos -= 1) { // goes from 180 degrees to 45 degrees
+      neckServo.setPos(pos);              // tell servo to go to position in variable 'pos'
+      delay(100);                       // waits 15ms for the servo to reach the position
+    }
+      for (pos = 45; pos >= 0; pos -= 1) { // goes from 45 degrees to 0 degrees
+      neckServo.setPos(pos);              // tell servo to go to position in variable 'pos'
+      delay(100);                       // waits 15ms for the servo to reach the position
+    }
+      for (pos = 0; pos <= 45; pos += 1) { // goes from 0 degrees to 180 degrees
+      neckServo.setPos(pos);              // tell servo to go to position in variable 'pos'
+      delay(100);                       // waits 15ms for the servo to reach the position
+    }
 }
