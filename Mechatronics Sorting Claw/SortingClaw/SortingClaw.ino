ab1c162c646477524e913a6a89df07e0b5999d5b
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-08-12 14:15:15 +1000

    work in progress

--- Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
+++ Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
@@ -4,28 +4,28 @@
 
 #define LED_PIN 13
 
-myServo baseServo(5);
-myServo elbowServo1(6);
-myServo elbowServo2(9);
-myServo neckServo(10);
-myServo clawServo(11);
+myServo baseServo(10);
+//myServo elbowServo1(6);
+//myServo elbowServo2(9);
+//myServo neckServo(10);
+//myServo clawServo(11);
 
-myLed myLed();
+//myLed myLed();
 
 void setup() {
   Serial.begin(9600);
   baseServo.init(90);
-  elbowServo1.init(90);
-  elbowServo2.init(90);
-  neckServo.init(90);
-  clawServo.init(90);
+  //elbowServo1.init(90);
+  //elbowServo2.init(90);
+  //neckServo.init(90);
+  //clawServo.init(90);
 
 }
 
 void loop() {
-  baseServo.setPos(45);
-  elbowServo1.setPos(45);
-  elbowServo2.setPos(45);
-  neckServo.setPos(45);
-  clawServo.setPos(45);
+  int pos = 0;
+  for (pos = 90; pos >= 45; pos -=1){
+      baseServo.setPos(pos);
+      delay(50);
+  }
 }
