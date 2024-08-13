95170175e646588fdb79d404cbe707009d3a1b15
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-08-14 09:40:34 +1000

    work in progress (servo can move to and from various positions)

--- Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
+++ Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
@@ -3,8 +3,10 @@
 #include "ArduinoGraphics.h"
 #include "Arduino_LED_Matrix.h"
 
-myServo baseServo(10);
-myServo elbowServo1(9);
+myServo baseServo(11);
+myServo elbowServo1(10);
+myServo elbowServo2(6);
+myServo clawServo(5);
 
 ArduinoLEDMatrix myLed;
 
@@ -17,29 +19,62 @@ void setup() {
    myLed.textScrollSpeed(100);
    myLed.stroke(0xFF, 0, 0);
    myLed.beginText(0, 1, 0xFF, 0, 0);
-   myLed.print("V00.00.01");
+   myLed.print("V00.00.08");
    myLed.endText(SCROLL_LEFT);
 
 //initialise servos
   baseServo.init(90);
   elbowServo1.init(90);
+  elbowServo2.init(90);
+  clawServo.init(120);
   
-//move servo to 180 degrees
   int pos = 0;
-  for  (int pos = 90; pos <= 180; pos += 1) {
+  for  (int pos = 90; pos <= 180; pos += 1) { //move baseServo to 180 degrees
     baseServo.setPos(pos);
     delay(15);
   }
 
-  delay (1000);
+  delay (100);
 
-// move servo to 45 degrees
-  for (int pos = 90; pos >= 180; pos -=1){
+  for (int pos = 90; pos >= 45; pos -=1){ // move elbowServo1 to 45  degrees
     elbowServo1.setPos(pos);
+    delay(15);
+  }
+
+delay(100);
+
+  for (int pos = 90; pos <= 135; pos +=1) { //move elbowServo2 to 120 degrees
+    elbowServo2.setPos(pos);
+    delay(15);
+  }
+
+delay(100);
+
+//open and close claw
+  clawServo.setPos(30);
+  delay(1000);
+  clawServo.setPos(120);
+
+delay(100);
+
+    for (int pos = 120; pos >= 90; pos -=1) { //move elbowServo2 back
+    elbowServo2.setPos(pos);
+    delay(15);
+  }
+
+delay(100);
+
+
+  for (int pos = 45; pos <= 90; pos +=1){ // move elbowServo1 back
+    elbowServo1.setPos(pos);
+    delay(15);
+  }
+
+
+  for  (int pos = 180; pos >= 90; pos -= 1) { //move baseServo back
+    baseServo.setPos(pos);
     delay(15);
   }
 }
 
-void loop() {
-
-}
+void loop(){}
