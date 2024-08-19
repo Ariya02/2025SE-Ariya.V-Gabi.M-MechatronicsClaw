927686011785557fc90f55936af97a97b79d6c7f
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-08-19 12:18:22 +1000

    Sorting Claw files rearranged

--- Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
+++ Mechatronics Sorting Claw/SortingClaw/SortingClaw.ino
@@ -1,68 +1,74 @@
-#include <Arduino.h>
-#include "robotServo.h"
+#include "clawServo.h"
+#include "hingeServo.h"
+#include "rotatingServo.h"
 #include "ArduinoGraphics.h"
 #include "Arduino_LED_Matrix.h"
 
-myServo baseServo(11);
-myServo elbowServo1(10);
-myServo elbowServo2(6);
-myServo clawServo(5);
+#define BASE_SERVO_PIN 11
+#define ELBOW1_SERVO_PIN 10
+#define ELBOW2_SERVO_PIN 6
+#define CLAW_SERVO_PIN 5
+
+rotateServo baseServo(BASE_SERVO_PIN);
+hingeServo elbowServo1(ELBOW1_SERVO_PIN); //bottom hinge servo
+hingeServo elbowServo2(ELBOW2_SERVO_PIN); //top hinge servo
+clawServo sortingClaw(CLAW_SERVO_PIN); 
 
 ArduinoLEDMatrix myLed;
 
 void setup() {
-  Serial.begin(9600);
-
   //Display Version
-   myLed.begin();
+  myLed.begin();
    myLed.textFont(Font_5x7);
    myLed.textScrollSpeed(100);
    myLed.stroke(0xFF, 0, 0);
    myLed.beginText(0, 1, 0xFF, 0, 0);
-   myLed.print("V00.02.01");
+   myLed.print("V00.02.04");
    myLed.endText(SCROLL_LEFT);
 
-//initialise servos
-  baseServo.init(90);
-  elbowServo1.init(90);
-  elbowServo2.init(90);
-  clawServo.init(65);
-  
-  //Pick up and relocate 1st cube
+   //initialise servos
+  baseServo.initRotate(90);
+  elbowServo1.initHinge(90);
+  elbowServo2.initHinge(90);
+  sortingClaw.initClaw(65);
+
+  //Pick up and relocate 1st claw
   elbowServo2.moveServo(150);
-  clawServo.closeClaw();
-  elbowServo2.resetPos();
+  sortingClaw.closeClaw();
+  elbowServo2.resetPos(); 
   baseServo.moveServo(180); // moves base to 180 degrees
   elbowServo1.moveServo(50); // moves elbow1 to 60 degrees
   elbowServo2.moveServo(140); // moves elbow2 to 130 degrees
-  clawServo.openClaw();
-  elbowServo2.resetPos();
+  sortingClaw.openClaw();
   elbowServo1.resetPos();
+  elbowServo2.resetPos();
   baseServo.resetPos();
 
-  //Pick up and relocate 2nd cube
+  //Pick up and relocate 2nd claw
   elbowServo2.moveServo(160);
-  clawServo.closeClaw();
+  sortingClaw.closeClaw();
   elbowServo2.resetPos();
   baseServo.moveServo(0);
   elbowServo1.moveServo(50); 
   elbowServo2.moveServo(140);
-  clawServo.openClaw();
-  elbowServo2.resetPos();
+  sortingClaw.openClaw();
   elbowServo1.resetPos();
+  elbowServo2.resetPos();
   baseServo.resetPos();
 
-  //Pick up and relocate 3rd cube
+  //Pick up and relocate 3rd claw
+  elbowServo1.moveServo(70);
   elbowServo2.moveServo(170);
-  clawServo.closeClaw();
+  sortingClaw.closeClaw();
   elbowServo2.resetPos();
+  elbowServo1.resetPos();
   baseServo.moveServo(45);
   elbowServo1.moveServo(50); 
   elbowServo2.moveServo(140);
-  clawServo.openClaw();
-  elbowServo2.resetPos();
+  sortingClaw.openClaw();
   elbowServo1.resetPos();
+  elbowServo2.resetPos();
   baseServo.resetPos();
 }
 
-void loop(){}
+void loop() {}
---
+++ TestClaw/TestClaw.ino
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+#include "robotServo.h"
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
+
+myServo baseServo(11);
+myServo elbowServo1(10);
+myServo elbowServo2(6);
+myServo clawServo(5);
+
+ArduinoLEDMatrix myLed;
+
+void setup() {
+  Serial.begin(9600);
+
+  //Display Version
+   myLed.begin();
+   myLed.textFont(Font_5x7);
+   myLed.textScrollSpeed(100);
+   myLed.stroke(0xFF, 0, 0);
+   myLed.beginText(0, 1, 0xFF, 0, 0);
+   myLed.print("V00.02.01");
+   myLed.endText(SCROLL_LEFT);
+
+//initialise servos
+  baseServo.init(90);
+  elbowServo1.init(90);
+  elbowServo2.init(90);
+  clawServo.init(65);
+  
+  //Pick up and relocate 1st cube
+  elbowServo2.moveServo(150);
+  clawServo.closeClaw();
+  elbowServo2.resetPos();
+  baseServo.moveServo(180); // moves base to 180 degrees
+  elbowServo1.moveServo(50); // moves elbow1 to 60 degrees
+  elbowServo2.moveServo(140); // moves elbow2 to 130 degrees
+  clawServo.openClaw();
+  elbowServo2.resetPos();
+  elbowServo1.resetPos();
+  baseServo.resetPos();
+
+  //Pick up and relocate 2nd cube
+  elbowServo2.moveServo(160);
+  clawServo.closeClaw();
+  elbowServo2.resetPos();
+  baseServo.moveServo(0);
+  elbowServo1.moveServo(50); 
+  elbowServo2.moveServo(140);
+  clawServo.openClaw();
+  elbowServo2.resetPos();
+  elbowServo1.resetPos();
+  baseServo.resetPos();
+
+  //Pick up and relocate 3rd cube
+  elbowServo2.moveServo(170);
+  clawServo.closeClaw();
+  elbowServo2.resetPos();
+  baseServo.moveServo(45);
+  elbowServo1.moveServo(50); 
+  elbowServo2.moveServo(140);
+  clawServo.openClaw();
+  elbowServo2.resetPos();
+  elbowServo1.resetPos();
+  baseServo.resetPos();
+}
+
+void loop(){}
